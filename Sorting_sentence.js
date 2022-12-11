5ff22a0cfd0cfa186a7d4315115dcecf0b1aba43
bete7512 <betekbebe@gmail.com>
2022-12-11 10:43:16 +0300

    messaged

--- Sorting_sentence.js
+++ Sorting_sentence.js
@@ -2,24 +2,22 @@
  * @param {string} s
  * @return {string}
  */
-var sortSentence = function(s) {
+var sortSentence = function (s) {
     var splitted = s.split(' ')
     arr = []
-    splitted.map((element)=>{
-        let string = [...element]
-        let index = parseInt(string[string.length - 1])
-        string.pop()
-      let word = string.join('')
-        arr.push({word:word,index:index})
+    splitted.map((element) => {
+        let index = parseInt(element[element.length - 1])
+        let sth = element.slice(0, -1)
+        arr.push({ word: sth, index: index })
     })
-    var str = []
-    for(let i = 1;i<=arr.length;i++){
-        for (let elemnt of arr){
-            if(elemnt.index==i){
-                str.push(elemnt.word)
-            }
-        }
+    var str = ''
+    for (let i = 1; i <= arr.length; i++) {
+        arr.map((element) => {
+                (element.index == i)? str = (i!=1) ? (str + " " + element.word) : element.word : console.log("bete");
+        })
     }
-    let value = str.join(' ')
-    return value
-    };
+    return str
+};
+
+let sth = sortSentence("Myself2 Me1 I4 and3")
+console.log(sth);
