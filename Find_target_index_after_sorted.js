cf6d4d363fe89c8414452fcf2f0724b92b388297
Bete Goshme <99134122+bete7512@users.noreply.github.com>
2022-12-12 17:09:24 +0300

    Update Find_target_index_after_sorted.js

--- Find_target_index_after_sorted.js
+++ Find_target_index_after_sorted.js
@@ -5,15 +5,7 @@
  */
 var targetIndices = function(nums,target) {
     let holder = []
-for(var i = 0; i < nums.length; i++){
-       for(var j = 0; j < ( nums.length - i -1 ); j++){
-     if(nums[j] > nums[j+1]){ 
-       var temp = nums[j]
-       nums[j] = nums[j + 1]
-    nums[j+1] = temp
-     }
-   }
- }
+    nums = nums.sort((a,b)=>a-b))
     console.log(nums)
     for (let i=0; i< nums.length;i++){
         if(target === nums[i]){
